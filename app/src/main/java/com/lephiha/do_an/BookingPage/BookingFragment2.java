cb6f8abc9ce76a1afb568651371efb20221fd019
lephiha <phihasky@gmail.com>
2025-02-20 04:20:56 +0700

    add bookingFragment2,3 + upload img

--- app/src/main/java/com/lephiha/do_an/BookingPage/BookingFragment2.java
+++ app/src/main/java/com/lephiha/do_an/BookingPage/BookingFragment2.java
@@ -1,4 +1,55 @@
 package com.lephiha.do_an.BookingPage;
 
-public class BookingFragment2 {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.fragment.app.Fragment;
+
+import com.lephiha.do_an.GuidePage.GuidePageActivity;
+import com.lephiha.do_an.HomePage.HomePageActivity;
+import com.lephiha.do_an.R;
+
+public class BookingFragment2 extends Fragment {
+
+    private AppCompatButton btnHowToExam;
+    private AppCompatButton btnHomepage;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //inflate the layout
+        View view = inflater.inflate(R.layout.fragment_booking2, container, false);
+
+        setupComponent(view);
+        setupEvent();
+
+        return view;
+    }
+
+    //setup cpn
+    private void setupComponent(View view) {
+        btnHomepage = view.findViewById(R.id.btnHomepage);
+        btnHowToExam = view.findViewById(R.id.btnHowToExam);
+    }
+
+    //set event
+    private void setupEvent() {
+        btnHomepage.setOnClickListener(view -> {
+            Intent intent = new Intent(requireContext(), HomePageActivity.class);
+            startActivity(intent);
+            requireActivity().finish();
+        });
+
+        btnHowToExam.setOnClickListener(view -> {
+            Intent intent = new Intent(requireContext(), GuidePageActivity.class);
+            startActivity(intent);
+            requireActivity().finish();
+        });
+    }
 }
--- app/src/main/java/com/lephiha/do_an/BookingPage/BookingFragment3.java
+++ app/src/main/java/com/lephiha/do_an/BookingPage/BookingFragment3.java
@@ -1,8 +1,63 @@
 package com.lephiha.do_an.BookingPage;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Canvas;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatButton;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.lephiha.do_an.Container.BookingPhotoDelete;
+import com.lephiha.do_an.Container.BookingPhotoUpload;
+import com.lephiha.do_an.Helper.Dialog;
+import com.lephiha.do_an.Helper.GlobaleVariable;
+import com.lephiha.do_an.Helper.LoadingScreen;
+import com.lephiha.do_an.Model.Photo;
+import com.lephiha.do_an.R;
+import com.lephiha.do_an.RecyclerView.BookingPhotoRecyclerView;
+import com.lephiha.do_an.configAPI.HTTPRequest;
+import com.lephiha.do_an.configAPI.HTTPService;
+
+import org.json.JSONObject;
+
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+
+import it.xabaras.android.recyclerview.swipedecorator.RecyclerViewSwipeDecorator;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
 public class BookingFragment3 extends Fragment {
 
@@ -13,5 +68,326 @@ public class BookingFragment3 extends Fragment {
     private AppCompatButton btnNext;
     private AppCompatButton btnUpload;
     private RecyclerView recyclerView;
+    private BookingPhotoRecyclerView adapter;
 
+    private LinearLayout layout;
+    private Context context;
+    private Activity activity;
+
+    private Map<String, String> header;
+    private GlobaleVariable globaleVariable;
+    private LoadingScreen loadingScreen;
+    private Dialog dialog;
+
+    private BookingViewModel viewModel;
+    private List<Photo> list;
+    private int photoId;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //Inflate the layout
+        View view = inflater.inflate(R.layout.fragment_booking3, container, false );
+
+        setupComponent(view);
+        setupViewModel();
+        setupEvent();
+        return view;
+    }
+
+    //setup component
+
+    private void setupComponent(View view) {
+        Bundle bundle = getArguments();
+        assert bundle != null;
+        bookingId = (String) bundle.get("bookingId");
+
+        context = requireContext();
+        activity = requireActivity();
+        globaleVariable = (GlobaleVariable) activity.getApplication();
+        header = globaleVariable.getHeaders();
+        dialog = new Dialog(context);
+        loadingScreen = new LoadingScreen(activity);
+
+        recyclerView = view.findViewById(R.id.recyclerView);
+        layout = view.findViewById(R.id.linearLayout);
+        btnUpload = view.findViewById(R.id.btnUpload);
+        btnNext = view.findViewById(R.id.btnNext);
+    }
+
+
+    //setup event
+
+    private void setupEvent() {
+        //btn upload
+        btnUpload.setOnClickListener(view -> {
+            verifyStoragePermissions(activity);
+
+            Intent intent = new Intent();
+            intent.setType("image/*"); // allows any img file type. change * to specific extension to limit it
+            intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, false);
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            openGalleryToPickPhoto.launch(intent);
+        });
+
+        //btn Next
+        btnNext.setOnClickListener(view -> {
+            String fragmentTag = "bookingFragment2";
+            BookingFragment2 nextFragment = new BookingFragment2();
+
+            requireActivity().getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.frameLayout, nextFragment, fragmentTag)
+                    .addToBackStack(fragmentTag)
+                    .commit();
+        });
+    }
+
+    // Storage Permissions
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static final String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+    }
+
+    //open gallerry to pick photo
+
+    private final ActivityResultLauncher<Intent> openGalleryToPickPhoto = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(), result -> {
+                if (result.getResultCode() == RESULT_OK) {
+                    Intent data = result.getData();
+                    assert data != null;
+                    Uri uri = data.getData();
+
+                    uploadPhotoToServer(uri);
+                }
+                else {
+                    System.out.println(TAG);
+                    System.out.println("Error - openGalleryToPickPhoto");
+                }
+            }
+    );
+
+    //create a http request to upload photo
+
+    private void uploadPhotoToServer(Uri uri) {
+        //1- setup file path
+        String[] projection = {MediaStore.Images.Media.DATA};
+        Cursor cursor = activity.getContentResolver().query(uri, projection,null, null, null);
+
+        int columnIndex = cursor.getColumnIndex(projection[0]);
+        cursor.moveToFirst();
+        String filePath = cursor.getString(columnIndex);
+        cursor.close();
+
+        //2. config new request
+        Retrofit service = HTTPService.getInstance();
+        HTTPRequest api = service.create(HTTPRequest.class);
+
+        //3
+        System.out.println("booking id: "+ bookingId);
+        RequestBody id = RequestBody.create(MediaType.parse("multipart/form-data"), bookingId);
+
+        File file = new File(Uri.parse(filePath).toString());
+        RequestBody requestBodyFile = RequestBody.create(MediaType.parse("multipart/form-data"), file);
+
+        //multipartBody.part is used to send also the actual file name
+        MultipartBody.Part actualFile = MultipartBody.Part.createFormData("file", file.getName(), requestBodyFile);
+
+        String accessToken = globaleVariable.getAccessToken();
+        String type = "Patient";
+        Call<BookingPhotoUpload> container = api.bookingPhotoUpload(accessToken, type, id, actualFile);
+
+
+        //4
+        container.enqueue(new Callback<BookingPhotoUpload>() {
+            @SuppressLint("NotifyDataSetChanged")
+            @Override
+            public void onResponse(@NonNull Call<BookingPhotoUpload> call,@NonNull Response<BookingPhotoUpload> response) {
+                if (response.isSuccessful()) {
+                    BookingPhotoUpload content = response.body();
+                    assert content != null;
+                    int result = content.getResult();
+                    String msg = content.getMsg();
+
+                    if (result == 1) {
+                        viewModel.bookingPhotoReadAll(header, bookingId);
+                        adapter.notifyDataSetChanged();
+                    }
+                    else {
+                        dialog.announce();
+                        dialog.btnOK.setOnClickListener(view -> dialog.close());
+                        dialog.show(R.string.attention, msg, R.drawable.ic_info);
+                    }
+                }
+                if (response.errorBody() != null) {
+                    try {
+                        JSONObject jObjError = new JSONObject(response.errorBody().string());
+                        System.out.println(jObjError);
+                    }
+                    catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<BookingPhotoUpload> call,@NonNull Throwable t) {
+                System.out.println(TAG);
+                System.out.println("Error");
+                t.printStackTrace();
+            }
+        });
+    }
+
+    //setup view model
+    private void setupViewModel() {
+        //declare
+        viewModel = new ViewModelProvider(this).get(BookingViewModel.class);
+        viewModel.instantiate();
+
+        //send request
+        viewModel.bookingPhotoReadAll(header, bookingId);
+        viewModel.getBookingPhotoReadAllResponse().observe((LifecycleOwner) context, response -> {
+            try {
+                int result = response.getResult();
+                // result = 1 => luu thong tin vao home
+                if (result == 1) {
+                    list = response.getData();
+                    System.out.println(TAG);
+                    System.out.println("photo size: "+ list.size());
+                    setupRecyclerView(list);
+                }
+                if (result == 0) {
+                    dialog.announce();
+                    dialog.show(R.string.attention, getString(R.string.check_your_internet_connection), R.drawable.ic_info);
+                    dialog.btnOK.setOnClickListener(view->{
+                        dialog.close();
+                        activity.finish();
+                    });
+                }
+            }
+            catch (Exception e) {
+                System.out.println(TAG);
+                System.out.println(e);
+
+                dialog.announce();
+                dialog.show(R.string.attention, getString(R.string.check_your_internet_connection), R.drawable.ic_info);
+                dialog.btnOK.setOnClickListener(view->{
+                    dialog.close();
+                    activity.finish();
+                });
+            }
+        });
+
+        //animation
+        viewModel.getAnimation().observe((LifecycleOwner) context, aBoolean -> {
+            if (aBoolean) loadingScreen.start();
+            else {
+                loadingScreen.stop();
+            }
+        });
+    }
+
+    //setup recyclerView
+    private void setupRecyclerView(List<Photo> list) {
+        adapter = new BookingPhotoRecyclerView(context, list);
+        recyclerView.setAdapter(adapter);
+
+        LinearLayoutManager manager = new LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(manager);
+
+        ItemTouchHelper helper = new ItemTouchHelper(callback);
+        helper.attachToRecyclerView(recyclerView);
+    }
+
+    //callback
+
+    ItemTouchHelper.SimpleCallback callback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+        @Override
+        public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+            return false;
+        }
+
+        @Override
+        public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+
+            int position = viewHolder.getAbsoluteAdapterPosition();
+            photoId = list.get(position).getId();
+
+            removePhotoFromList(photoId);
+            list.remove(position);
+            adapter.notifyItemRemoved(position);// trigger event one photo deleted from list
+        }
+
+        @Override
+        public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+
+            new RecyclerViewSwipeDecorator.Builder(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+                    .addBackgroundColor(ContextCompat.getColor(context, R.color.colorRed))
+                    .addActionIcon(R.drawable.ic_delete)
+                    .create()
+                    .decorate();
+
+            super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+        }
+    };
+
+    //send request to remove photo from list
+
+    private void removePhotoFromList(int photoId) {
+        //2
+        Retrofit service = HTTPService.getInstance();
+        HTTPRequest api = service.create(HTTPRequest.class);
+
+        //3
+        Call<BookingPhotoDelete> container = api.bookingPhotoDelete(header, photoId);
+
+        //4
+        container.enqueue(new Callback<BookingPhotoDelete>() {
+            @Override
+            public void onResponse(@NonNull Call<BookingPhotoDelete> call,@NonNull Response<BookingPhotoDelete> response) {
+                if (response.isSuccessful()) {
+                    BookingPhotoDelete content = response.body();
+                    assert content != null;
+                    int result = content.getResult();
+                    Snackbar snackbar;
+                    if (result == 1) {
+                        snackbar= Snackbar.make(layout, "Success", Snackbar.LENGTH_SHORT);
+                    }
+                    else {
+                        snackbar = Snackbar.make(layout, "Fail", Snackbar.LENGTH_SHORT);
+                    }
+                    snackbar.show();
+                }
+                if (response.errorBody() != null) {
+                    try {
+                        JSONObject jObjError = new JSONObject(response.errorBody().string());
+                        System.out.println(jObjError);
+                    }
+                    catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<BookingPhotoDelete> call,@NonNull Throwable t) {
+                System.out.println(TAG);
+                System.out.println("Error from function removePhotoFromList(): " + t.getMessage());
+            }
+        });
+    }
 }
